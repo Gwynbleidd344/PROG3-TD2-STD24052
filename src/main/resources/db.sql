1c4cca8fae40ce67784bdb58c342ccee49f6a833
Vaniaina <hei.vaniaina@gmail.com>
2025-12-17 14:08:24 +0300

    add: basic class

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE mini_dish_db;
-CREATE USER mini_dish_db_manager;
+CREATE USER mini_dish_db_manager WITH PASSWORD '123456';
 GRANT CONNECT ON DATABASE mini_dish_db TO mini_dish_db_manager;
 GRANT CREATE ON DATABASE mini_dish_db TO mini_dish_db_manager;
 GRANT SELECT, INSERT, UPDATE, DELETE ON Ingredient TO mini_dish_db_manager;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
-CREATE TYPE Category AS ENUM (VEGETABLE,ANIMAL, MARINE, DAIRY, OTHER);
-CREATE TYPE Dish_type AS ENUM (START, MAIN, DESSERT);
+CREATE TYPE Category AS ENUM ('VEGETABLE','ANIMAL','MARINE', 'DAIRY', 'OTHER');
+CREATE TYPE Dish_type AS ENUM ('START', 'MAIN', 'DESSERT');
+CREATE TABLE Dish (
+    id serial PRIMARY KEY NOT NULL,
+    name varchar(100) NOT NULL ,
+    dish_type Dish_type NOT NULL
+);
 CREATE TABLE Ingredient (
     id serial PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
-    price numeric(10,2),
-    category Category,
+    price numeric(10,2) NOT NULL ,
+    category Category NOT NULL ,
     id_dish int,
     CONSTRAINT fk_id_dish foreign key (id_dish) references Dish(id)
-);
-CREATE TABLE Dish (
-    id serial PRIMARY KEY NOT NULL,
-    name varchar(100),
-    dish_type Dish_type
 );
